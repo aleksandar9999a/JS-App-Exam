1e0b4df57b1be54581d4d5885914112d14a59a69
aleksandar9999a <aleksandar9999a@gmail.com>
2019-12-07 10:40:33 +0200

    Added Login Functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,17 @@
 import { loadHome } from './controllers/homeController.js';
 import { loadRegisterForm, createUser } from './controllers/registerController.js';
-
-const partials = {
-    nav: 'components/common/nav.hbs',
-    footer: 'components/common/footer.hbs'
-};
+import { loadLoginForm, login } from './controllers/loginController.js';
 
 const app = Sammy('body', function () {
     this.use('Handlebars', 'hbs');
 
     this.get('/', loadHome)
     this.get('/register', loadRegisterForm)
+    this.get('/login', loadLoginForm)
+    
 
     this.post('/register', createUser)
+    this.post('/login', login)
 })
 
 app.run();
--- scripts/controllers/sessionController.js
+++ scripts/controllers/sessionController.js
@@ -1,7 +1,7 @@
 export function getSessionInfo(ctx) {
     ctx.loggedIn = sessionStorage.getItem('authtoken') !== null;
     if (ctx.loggedIn) {
-        ctx.name = sessionStorage.getItem('name');
+        ctx.username = sessionStorage.getItem('username');
     }
 }
 
@@ -9,7 +9,6 @@ export function setSessionInfo(data) {
     sessionStorage.setItem('authtoken', data._kmd.authtoken);
     sessionStorage.setItem('username', data.username);
     sessionStorage.setItem('id', data._id);
-    sessionStorage.setItem('name', `${data.firstName} ${data.lastName}`);
 }
 
 export function clearSessionData(){
